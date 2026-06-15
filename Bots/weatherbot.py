3f5dd6d8147869f8f0f2e96eac2fe923ee64b8f0
alinooralian <alinooralian.85@gmail.com>
2026-06-15 13:26:22 +0330

    A Telegram bot that receives the location of a place from the user and sends the temperature and humidity of that location

---
+++ Bots/weatherbot.py
@@ -0,0 +1,21 @@
+import telebot
+import requests
+
+API_TOKEN = "7326074022:AAEGzBMTs48aE93Ro4jps3HCtvepabu_3c0"
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello! please send a location...")
+
+@bot.message_handler(content_types=['location'])
+def handle_location(message):
+    location = [message.location.latitude, message.location.longitude]
+    respone = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={location[0]}&longitude={location[1]}&current=temperature_2m,relative_humidity_2m")
+    bot.reply_to(message, f"Temp: {respone.json()["current"]["temperature_2m"]}\nHumidity: {respone.json()["current"]["relative_humidity_2m"]}")
+
+
+
+bot.infinity_polling()
